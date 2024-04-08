0c1615357b4be2b038ac1263e7b5f53976ddb755
Oleksandr Laptiev <olaptiev@lenovo.com>
2024-04-08 20:48:15 +0300

    Added allowed origin to cors mappings

--- src/main/java/com/example/farmhelper/config/WebMvcConfig.java
+++ src/main/java/com/example/farmhelper/config/WebMvcConfig.java
@@ -28,7 +28,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000", "https://laptiev-o.pp.ua")
             .allowedMethods("*");
     }
 
